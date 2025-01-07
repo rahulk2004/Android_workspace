c6470d02c1f1fadcfa507a55ffc9fe14d2f68ae1
Rahul Kanara <rahulkanara3089@gmail.com>
2025-01-07 11:06:19 +0530

    commit

---
+++ Module 5/ProjectBloodbank/app/src/main/java/com/example/projectbloodbank/GlobalVariables.kt
@@ -0,0 +1,22 @@
+package com.example.projectbloodbank
+
+object GlobalVariables {
+
+    var bloodBankId = 0
+    var bloodBankName: String? = null
+    var bloodBankLocation: String? = null
+    var updateFlag = ""
+
+    var bloodRequestId: Int = 0
+    var userId: String = ""
+    var userName: String = ""
+    var bloodGroup: String = ""
+    var contact: String = ""
+    var address: String = ""
+    var requestDate: String = ""
+    var status: String = ""
+
+
+
+
+}
